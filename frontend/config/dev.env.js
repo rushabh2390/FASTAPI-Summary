55db4889f1f38856cafaadaa57633a12ec37eb4d
rushabh2390 <er.rushabhdoshi@gmail.com>
2022-04-12 09:13:56 +0530

    update envieonement variable kubernates file

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV:  process.env.NODE_ENV || "development",
-  API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000"
+  NODE_ENV:  process.env.NODE_ENV || '"development"',
+  API_ENDPOINT: process.env.API_ENDPOINT || '"http://localhost:8000"'
 })
