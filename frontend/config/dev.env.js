60b1176bb65e67a29ecfddc8dc1306e3632bf521
rushabh2390 <er.rushabhdoshi@gmail.com>
2022-04-14 09:27:17 +0530

    update dockerfile

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -1,8 +1,8 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+const prodEnv = require('./dev.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV:  process.env.NODE_ENV || '"development"',
-  API_ENDPOINT: process.env.API_ENDPOINT || '"http://localhost:8000"'
+  NODE_ENV:  process.env.NODE_ENV || "development",
+  API_ENDPOINT: process.env.API_ENDPOINT || "http://localhost:8000"
 })
