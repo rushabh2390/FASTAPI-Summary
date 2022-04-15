fc6d9963f116045c9d7e97c80fc2bbdfbfb9e41a
rushabh2390 <er.rushabhdoshi@gmail.com>
2022-04-15 09:12:58 +0530

    rneame old frontend to new and create frontend

---
+++ frontend(old)/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <file-upload></file-upload>
+  </div>
+</template>
+
+<script>
+import FileUpload from './components/FileUpload'
+export default {
+  components: { FileUpload },
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
