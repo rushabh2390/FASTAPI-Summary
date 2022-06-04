4c3a8fb100c1ebf1a2ea4763669bbeae280033f7
rushabh2390 <er.rushabhdoshi@gmail.com>
2022-06-04 18:05:04 +0530

    add output image and change config.js value

--- frontend/public/static/config.js
+++ frontend/public/static/config.js
@@ -1,6 +1,6 @@
 const config = (() => {
   return {
-    "VUE_APP_TITLE":"check value",
+    "VUE_APP_TITLE":"MetaData & Summary Generater",
     "VUE_APP_API_BACKEND":"http://localhost:8000"
   };
 })();
